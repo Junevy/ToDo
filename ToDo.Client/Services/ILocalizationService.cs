d8666f8649ccd193fa049b1568777ca90acd4a1f
Junevy <junev.chung@gmail.com>
2025-12-12 00:05:33 +0800

    Fixed localization logic.

--- ToDo.Client/Services/ILocalizationService.cs
+++ ToDo.Client/Services/ILocalizationService.cs
@@ -1,7 +1,15 @@
 ﻿public interface ILocalizationService
 {
-    void SetCulture(string cultureName);
+    /// <summary>
+    /// Current Culture
+    /// </summary>
     string CurrentCulture { get; }
+
+    /// <summary>
+    /// Switch Culture
+    /// </summary>
+    /// <param name="cultureName">culture name, such as "en-US", "中文-简体" etc.</param>
+    void SetCulture(string cultureName);
 }
 
 
